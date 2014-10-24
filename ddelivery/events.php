b23f26378d54ceb9649b89aaee1fb35701e790cc
mrozk <mrozk2012@gmail.com>
2014-10-24 12:01:31 +0300

    Снес все

--- ddelivery/events.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-new umiEventListener('order-status-changed', 'ddelivery', 'onCmsOrderFinish');
-new umiEventListener('systemModifyPropertyValue', 'ddelivery', 'onModifyStatusValue');
-new umiEventListener('systemModifyObject', 'ddelivery', 'onModifyStatusObject');
--- ddelivery/includes.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once 'classes/modules/emarket/includes.php';
